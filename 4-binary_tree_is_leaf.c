0d816bb1fa04326c46fcedd96dfa44a150d19c4f
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-05-30 23:49:50 -0700

    Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if a node is leaf
+ * @node: node to check
+ * Return: 1 if is a leaf else 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+	return (1);
+}
