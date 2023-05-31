8a790bc02f8776c67f1453e1254a2cc64b7dcf61
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-05-30 23:44:58 -0700

    Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,3 +1,4 @@
+#include "binary_trees.h"
 /**
  * binary_tree_delete - deletes an entire binary tree
  * @tree: a binary tree to delete
@@ -7,7 +8,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		binary_tree_delete(tree->leftt);
+		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
 	}
