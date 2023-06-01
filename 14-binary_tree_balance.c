3e23d52126a8c90f1d7e487ca27d4717c490ad52
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-01 10:32:11 -0700

    Balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: root node of the tree
+ * Return: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
+}
