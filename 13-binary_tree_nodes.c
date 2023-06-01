fd89e8462c7d8c3297a971a9b83b130ffee77d22
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-01 10:20:51 -0700

    Nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the number of nodes in a binary tree
+ * @tree: root node of the tree
+ * Return: number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
