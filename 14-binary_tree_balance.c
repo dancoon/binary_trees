67b720fd69c00755aa742f2a9e153cd3543034d6
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-01 10:35:39 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,3 +12,20 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (0);
 }
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: if the tree is NULL, must return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree)
+	{
+		l = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
+		r = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
+		return (l > r ? l : r);
+	}
+	return (0);
+}
